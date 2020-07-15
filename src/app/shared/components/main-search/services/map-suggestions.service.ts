59f90abff4067729c0791c27c53de1fe3106add7
mishotek <mtekt16@freeuni.edu.ge>
2020-07-15 23:46:16 +0400

    # fixed issue with translations # fixed issue with pagination

--- src/app/shared/components/main-search/services/map-suggestions.service.ts
+++ src/app/shared/components/main-search/services/map-suggestions.service.ts
@@ -1,11 +1,40 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
 import {MapSuggestion} from '../../../models/map-suggestion.model';
 import {debounceTime, map, switchMap} from 'rxjs/operators';
 import {DataService} from '../../../../core/services/data.service';
 import {Urls} from '../../../../../assets/configs/urls';
+import {LanguageService} from '../../../../core/services/language.service';
+import {Language} from '../../../../core/models/language';
 
-const DEFAULT_SUGGESTIONS: Array<MapSuggestion> = [
+const DEFAULT_SUGGESTIONS_EN: Array<MapSuggestion> = [
+  {
+    description: 'Tbilisi',
+    id: 'ChIJa2JP5tcMREARo25X4u2E0GE'
+  },
+  {
+    description: 'Chavchavadze ave',
+    // tslint:disable-next-line:max-line-length
+    id: 'ElPhg5jhg5rhg5jhg5Ag4YOt4YOQ4YOV4YOt4YOQ4YOV4YOQ4YOr4YOY4YOhIOGDkuGDkOGDm-GDluGDmOGDoOGDmCwgVGJpbGlzaSwgR2VvcmdpYSIuKiwKFAoSCS--WjUyc0RAEZSxs_zDnbtsEhQKEglrYk_m1wxEQBGjblfi7YTQYQ'
+  },
+  {
+    description: 'Vazha-pshavela ave',
+    // tslint:disable-next-line:max-line-length
+    id: 'Ek3hg5Xhg5Dhg5_hg5At4YOk4YOo4YOQ4YOV4YOU4YOa4YOQ4YOhIOGDkuGDkOGDm-GDluGDmOGDoOGDmCwgVGJpbGlzaSwgR2VvcmdpYSIuKiwKFAoSCWuR3EEOc0RAEas8vsKCjQU3EhQKEglrYk_m1wxEQBGjblfi7YTQYQ'
+  },
+  {
+    description: 'Mikheil tamarashvili ave',
+    // tslint:disable-next-line:max-line-length
+    id: 'Elbhg5vhg5jhg67hg5Thg5jhg5og4YOX4YOQ4YOb4YOQ4YOg4YOQ4YOo4YOV4YOY4YOa4YOY4YOhIOGDpeGDo-GDqeGDkCwgVGJpbGlzaSwgR2VvcmdpYSIuKiwKFAoSCSGg004Wc0RAEVPaNDsZQFcaEhQKEglrYk_m1wxEQBGjblfi7YTQYQ'
+  },
+  {
+    description: 'Khazbegi ave',
+    // tslint:disable-next-line:max-line-length
+    id: 'EkDhg6fhg5Dhg5bhg5Hhg5Thg5Lhg5jhg6Eg4YOS4YOQ4YOb4YOW4YOY4YOg4YOYLCBUYmlsaXNpLCBHZW9yZ2lhIi4qLAoUChIJAbBNyx5zREARSG6KB8ceZjMSFAoSCWtiT-bXDERAEaNuV-LthNBh'
+  }
+];
+
+const DEFAULT_SUGGESTIONS_GE: Array<MapSuggestion> = [
   {
     description: 'თბილისი',
     id: 'ChIJa2JP5tcMREARo25X4u2E0GE'
@@ -39,7 +68,8 @@ export class MapSuggestionsService {
 
   private readonly MAX_SUGGESTION_COUNT = 5;
 
-  constructor(private dataService: DataService) { }
+  constructor(private dataService: DataService,
+              private languageService: LanguageService) { }
 
   public getSuggestions(queries: Observable<{input: string, sessiontoken: string}>): Observable<Array<MapSuggestion>> {
     return queries.pipe(
@@ -55,7 +85,13 @@ export class MapSuggestionsService {
         .get(`${Urls.PLACE_SUGGESTIONS}/${sessiontoken}/${input}`);
     }
 
-    return of(DEFAULT_SUGGESTIONS);
+    return this.languageService.getSelectedLanguage()
+      .pipe(map(language => {
+        if (language === Language.ge) {
+          return DEFAULT_SUGGESTIONS_GE;
+        }
+        return DEFAULT_SUGGESTIONS_EN;
+      }));
   }
 
   private limitSuggestions(suggestions: Array<MapSuggestion>): Array<MapSuggestion> {
